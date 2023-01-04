537d6fe7a94d3121fbc1f79931bde4e157f8227f
Grupo Altum <grupoaltum@MacBook-Pro-de-Grupo.local>
2023-01-04 15:34:35 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,52 @@
+# PHP2JS()
+Lea las variables de PHP LARAVEL en un archivo externo de JAVASCRIPT sin necesidad de hacer peticiones AJAX, FETCH o AXIOS, use las mismas variables del archivo retornado por el controlador.
+
+[![N|Solid](https://i.ibb.co/ZLzQTpm/Firma-Git-Hub.png)](#)
+
+## Instalación
+1. Instalar a traves de composer.
+```sh
+composer require rmunate/php2js
+```
+
+2.	Presentar el Proveedor en el archivo config\app.php.
+
+```sh
+'providers' => [
+	//
+	Rmunate\Php2js\PHP2JSServiceProvider::class,
+],
+```
+
+## Uso
+En la vista antes de llamar el archivo externo de JavaScript, se debe poner la directiva 
+```sh
+@__PHP()
+<script src="{{ asset('..............js') }}"></script>
+```
+
+## Métodos
+
+| FUNCIÓN | DESCRIPCIÓN |
+| ------ | ------ |
+| __PHP().all() | Retorna toda la información de las variables en uso dentro de un objeto. |
+| __PHP().vars() | Retorna exclusivamente las variables seteadas en PHP en un objeto. |
+| __PHP().errors() | Retorna los errores de captura de las variables de PHP. |
+| __PHP().path() | Retorna el nombre del archivo desde el cual se está capturando las variables. |
+| __PHP().env() | Retorna variables de entorno generadas en la acción de tomar las variables (No retorna los datos el ENV de laravel), comúnmente no se generan por lo cual retorna vacío. |
+| __PHP().app() | Retorna variables guardadas en el objeto principal App. |
+| __PHP().route() | Retorna el nombre de la ruta según el archivo de rutas de laravel. |
+| __PHP().fullUrl() | Retorna la URL completa, en caso de envíos tipo GET, retorna la URL con los parámetros. |
+| __PHP().url() | Retorna la URL completa, sin parámetros |
+| __PHP().root() | Retorna el dominio en uso. |
+| __PHP().token() | Retorna un CSRF TOKEN. |
+| __PHP().tokenMeta() | Retorna una etiqueta meta con el CSRF TOKEN. |
+| __PHP().tokenInput() | Retorna un input oculto con el CSRF TOKEN. |
+
+## Desarrollador
+
+Ingeniero, Raúl Mauricio Uñate Castro
+raulmauriciounate@gmail.com
+
+## Licencia
+MIT
---
+++ src/PHP2JSServiceProvider.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace Rmunate\Php2js;
+
+/*
+|--------------------------------------------------------------------------
+| Envio de variables de PHP a JS en vistas Blade
+|--------------------------------------------------------------------------
+| Autor: Raul Mauricio Uñate Castro
+| V 3.1.1 : 03-03-2022
+|--------------------------------------------------------------------------
+|
+*/
+
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Request;
+use Illuminate\Support\ServiceProvider;
+
+class PHP2JSServiceProvider extends ServiceProvider
+{
+    /* Register services. */
+    public function register(){
+    }
+
+    /**
+     * Bootstrap services.
+     * @return void
+     */
+    public function boot(){
+
+        /* Pasar Variables de PHP a JS */
+        Blade::directive('__PHP', function () {
+
+            return
+            "<script type='text/javascript'>
+
+            class PHP {
+
+                constructor() {
+                    this.data = {
+                        vars: <?php echo json_encode(get_defined_vars()); ?>,
+                        route: <?php echo json_encode(Illuminate\Support\Facades\Route::currentRouteName()); ?>,
+                        fullUrl: <?php echo json_encode(Illuminate\Support\Facades\Request::fullUrl()); ?>,
+                        url: <?php echo json_encode(Illuminate\Support\Facades\Request::url()); ?>,
+                        root: <?php echo json_encode(Illuminate\Support\Facades\Request::root()); ?>,
+                        token: <?php echo json_encode(csrf_token()); ?>
+                    }
+                }
+
+                all(){
+                    return this.data;
+                }
+
+                vars(){
+                    delete this.data.vars.app;
+                    delete this.data.vars.__data;
+                    delete this.data.vars.errors;
+                    delete this.data.vars.__env;
+                    delete this.data.vars.__path;
+                    return this.data.vars;
+                }
+
+                errors(){
+                    return this.data.vars.errors;
+                }
+
+                path(){
+                    return this.data.vars.__path;
+                }
+
+                env(){
+                    return this.data.vars.__env;
+                }
+
+                app(){
+                    return this.data.vars.app;
+                }
+
+                route(){
+                    return this.data.route;
+                }
+
+                fullUrl(){
+                    return this.data.fullUrl;
+                }
+
+                url(){
+                    return this.data.url;
+                }
+
+                root(){
+                    return this.data.root;
+                }
+
+                token(){
+                    return this.data.token;
+                }
+
+                tokenMeta(){
+                    return '<meta name=' + String.fromCharCode(34) + ' csrf-token' + String.fromCharCode(34) + ' content=' + String.fromCharCode(34) + this.data.token + String.fromCharCode(34) + '>';
+                }
+
+                tokenInput(){
+                    return '<input type' + String.fromCharCode(34) + 'hidden' + String.fromCharCode(34) + ' name=' + String.fromCharCode(34) + '_token' + String.fromCharCode(34) + ' value=' + String.fromCharCode(34) + this.data.token + String.fromCharCode(34) + '/>';
+                }
+
+            }
+
+            function __PHP() {
+                const php2js = new PHP();
+                return php2js;
+            };
+
+            </script>";
+        });
+    }
+}
